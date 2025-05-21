037dc99c6a93931f9324338a568332b681de3ed4
Stalyn Noboa <sonoboa@hotmail.com>
2025-05-21 02:19:42 -0400

    faq

---
+++ src/app/faq/page.jsx
@@ -0,0 +1,10 @@
+export default function page() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-700 mb-4">FAQ</h1>
+      <p className="text-gray-700 mb-4">
+        This website was designed as part 2 of the Midterm Exam for Accounting 343, taken at Queens College
+      </p>
+    </>
+  );
+}
