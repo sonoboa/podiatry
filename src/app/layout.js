8ef13c4b425a4c2f9efc38e383439094d15f0006
Stalyn Noboa <sonoboa@hotmail.com>
2025-05-22 00:27:58 -0400

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Menubar from "./components/Menubar";
+import menubar from "./components/menubar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
